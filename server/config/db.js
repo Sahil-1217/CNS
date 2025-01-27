0aa853bc86e2507c104318dbf3af2393677d4592
Sahil <137210220+Sahil-1217@users.noreply.github.com>
2025-01-27 23:12:30 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const connectDB = async()=> {
   try {
     mongoose.set('strictQuery', false);
     //const conn = await mongoose.connect(process.env.MONGODB_URI);
-    const conn = await mongoose.connect("mongodb://localhost:27017");
+    const conn = await mongoose.connect("mongodb://localhost:27017/test");
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
